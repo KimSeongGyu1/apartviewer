9cb593127cdf554313d1d00b08415ef74f56d110
ksg <luckykimsk@gmail.com>
2023-05-11 22:35:18 +0900

    최초 커밋

---
+++ src/main/java/com/apartviewer/ApartviewerApplication.java
@@ -0,0 +1,13 @@
+package com.apartviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartviewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApartviewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apartviewer/ApartviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apartviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartviewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
